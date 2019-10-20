f08d201f834ef3eaba4628a3fc56229903c38303
Sean <seanpai96@gmail.com>
2019-10-21 00:12:07 +0800

    Update a005.py

--- Zerojudge/Python/a005.py
+++ Zerojudge/Python/a005.py
@@ -1,3 +1,7 @@
 n = int(input())
-for i in n:
-  
+for i in range(n):
+    a,b,c,d = map(int,input().split())
+    if b-a == d-c:
+        print(a,b,c,d,(d+(b-a)))
+    elif b/a == d/c:
+        print(a,b,c,d,(d*(int(b/a))))
